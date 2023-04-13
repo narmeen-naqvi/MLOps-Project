5eba8fa4504298971a4f913b3541cf2c82caa6a7
Adeen <i190553@nu.edu.pk>
2023-04-14 00:22:06 +0500

    flask image

--- app/src/main/java/com/adeenayub/idraakphase1/RecordingScreen.java
+++ app/src/main/java/com/adeenayub/idraakphase1/RecordingScreen.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
@@ -25,16 +31,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RecordingScreen extends AppCompatActivity {
 ImageView record,camera,uploadpicture;
@@ -49,8 +55,8 @@ VideoView uploadvideo;
 private AlertDialog.Builder dialogBuilder;
 private AlertDialog dialog;
 private TextView text_popupTitle, text_popupDescription,help_popupTitle,help_popupDescription;
-private Button text_cancelButton,help_cancelButton;
-String predictedclass;
+private Button text_cancelButton,help_cancelButton,connect;
+String selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,32 +67,98 @@ String predictedclass;
         camera = findViewById(R.id.camera_button);
         uploadpicture = findViewById(R.id.taken_picture);
         uploadvideo = findViewById(R.id.taken_video);
+        connect = findViewById(R.id.connectServer);
 
         camera.setOnClickListener(v -> {
             //using this for image capture for now
             Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show();
-            if (isCameraPresentInPhone()){
-                Log.i("Image_capture_tag","Camera is detected");
+            if (isCameraPresentInPhone()) {
+                Log.i("Image_capture_tag", "Camera is detected");
                 getCameraPermission();
                 imageCapture();
-            }
-            else{
-                Log.i("Image_capture_tag","Camera is not detected");
+            } else {
+                Log.i("Image_capture_tag", "Camera is not detected");
             }
         });
 
         record.setOnClickListener(v -> {
             Toast.makeText(this, "Record clicked", Toast.LENGTH_SHORT).show();
-            if (isCameraPresentInPhone()){
-                Log.i("Video_record_tag","Camera is detected");
+            if (isCameraPresentInPhone()) {
+                Log.i("Video_record_tag", "Camera is detected");
                 getCameraPermission();
                 recordvideo();
+            } else {
+                Log.i("Video_record_tag", "Camera is not detected");
             }
-            else{
-                Log.i("Video_record_tag","Camera is not detected");
+        });
+
+        connect.setOnClickListener(v -> {
+            String postUrl = "http://192.168.1.6:5000/";
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+            // Read BitMap by file path
+            Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath, options);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            RequestBody postBodyImage = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+                    .build();
+
+            TextView responseText = findViewById(R.id.responseText);
+            responseText.setText("Please wait ...");
+
+            postRequest(postUrl, postBodyImage);
+
+            //String postBodyText = "Hello";
+            //MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
+            //RequestBody postBody = RequestBody.create(mediaType, postBodyText);
+            //postRequest(postUrl, postBody);
+        });
+    }
+    private void postRequest(String postUrl, RequestBody postBody) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+                Toast.makeText(RecordingScreen.this, "Failed to connect to server", Toast.LENGTH_SHORT).show();
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        responseText.setText("Failed to Connect to Server");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        try {
+                            responseText.setText(response.body().string());
+                            Toast.makeText(RecordingScreen.this, "Connected to server", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
     }
+
 //menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -213,8 +285,8 @@ String predictedclass;
 
         return prediction;
     }*/
-public String predictLetter(Uri imageUri) throws IOException, JSONException {
-    URL url = new URL("http://127.0.0.1:5000/predict");
+/*public String predictLetter(Uri imageUri) throws IOException, JSONException {
+    URL url = new URL("http://127.0.0.1:5000/");
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
     connection.setRequestMethod("POST");
     connection.setRequestProperty("Content-Type", "image/jpeg");
@@ -242,7 +314,8 @@ public String predictLetter(Uri imageUri) throws IOException, JSONException {
     String prediction = jsonResponse.getString("prediction");
 
     return prediction;
-}
+}*/
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -276,6 +349,9 @@ public String predictLetter(Uri imageUri) throws IOException, JSONException {
                 uploadpicture.setImageBitmap(finalphoto);
                 //predict image
                 imagePath = data.getData();
+                selectedImagePath = getPath(getApplicationContext(), imagePath);
+                Toast.makeText(getApplicationContext(), selectedImagePath, Toast.LENGTH_LONG).show();
+
                 Log.i("Image_capture_tag","Image captured at path " + imagePath);
                 Toast.makeText(this, "Image captured successfully", Toast.LENGTH_SHORT).show();
                 /*try {
@@ -299,5 +375,102 @@ public String predictLetter(Uri imageUri) throws IOException, JSONException {
                 Toast.makeText(this, "Image captured failed", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+    // Implementation of the getPath() method and all its requirements is taken from the StackOverflow Paul Burke's answer: https://stackoverflow.com/a/20559175/5426539
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
